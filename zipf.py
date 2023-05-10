03de6eafde4b1730ef2a616fee9c60debad5c722
Jose Zamora <123494107+cosmo-novah@users.noreply.github.com>
2023-05-10 09:47:04 -0700

    altered zipf.py

--- zipf.py
+++ zipf.py
@@ -1,11 +1,12 @@
-collection = 1000
+collection = input("Please enter the number of movies in your collection: ")
 total = 0
 div = 0
 
-for x in range(1, collection + 1):
+for x in range(1, int(collection) + 1):
     div += 1/x
-    print (x, div)
+    #print (x, div)
 else:
     total = 1/div
 
-print ("Total:", total)
+print ("Zipf's Law for a total movie collection of", collection, 
+       "is:", total)
